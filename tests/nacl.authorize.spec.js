1fc5e552c2c18dcd83e0293b61e11034528bcbb5
andela-thomas <thomas.nyambati@andela.com>
2016-04-07 13:47:15 +0300

    Adds tests on express authorize middleware for action "deny"

--- tests/nacl.authorize.spec.js
+++ tests/nacl.authorize.spec.js
@@ -5,6 +5,54 @@ var httpMocks = require('node-mocks-http');
 describe('Acl middleware for express', function() {
   var req, res, next;
   /**
+   * Home route
+   * traffic to this route should always be allowed
+   */
+
+  describe('Home route testing', function() {
+    beforeEach(function(done) {
+      acl.config();
+
+      req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/',
+        params: {
+          id: 42
+        }
+      });
+
+      res = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+
+      req.decoded = {};
+      req.session = {};
+      done();
+    });
+
+    it('should allow traffic for the home route', function(done) {
+      //{ status: 403, success: false, error: 'ACCESS DENIED' }
+      next = function() {
+        res.send({
+          status: 200,
+          success: true,
+          message: 'ACCESS GRANTED'
+        });
+      };
+
+      acl.authorize(req, res, next);
+      var data = res._getData();
+
+      assert(data, true);
+      assert.deepEqual(data.status, 200);
+      assert.deepEqual(data.success, true);
+      assert.deepEqual(data.message, 'ACCESS GRANTED');
+      done();
+    });
+
+  });
+
+  /**
    * Test if it denies traffic,  when roles are not defined
    */
 
@@ -37,7 +85,7 @@ describe('Acl middleware for express', function() {
 
     it('should block traffic if no role is defined', function(done) {
       //{ status: 403, success: false, error: 'ACCESS DENIED' }
-      acl.authorize(req, res, done);
+      acl.authorize(req, res, next);
       var data = res._getData();
       assert(data, true);
       assert(typeof data, 'object');
@@ -48,13 +96,7 @@ describe('Acl middleware for express', function() {
     });
 
     it('should allow traffic for the user when role is defined on /api/user/42', function(done) {
-      next = function() {
-        res.send({
-          status: 200,
-          success: true,
-          message: 'ACCESS GRANTED'
-        });
-      };
+
       req.decoded.role = 'user';
       /**
        * Ensure that the allowed is false;
@@ -78,17 +120,19 @@ describe('Acl middleware for express', function() {
   });
 
   /**
+   * user policy.
+   *
    {
-      "group": "user",
-      "permissions": [{
-        "resource": "users",
-        "methods": [
-              "POST",
-              "GET",
-              "PUT"
-        ]
-      }],
-      "action": "allow"
+    "group": "user",
+    "permissions": [{
+      "resource": "users",
+      "methods": [
+        "POST",
+        "GET",
+        "PUT"
+      ]
+    }],
+    "action": "allow"
    }
    */
   describe('Policy traffic restriction testing based on action "allow"', function() {
@@ -206,6 +250,151 @@ describe('Acl middleware for express', function() {
       assert.deepEqual(data.status, 403);
       assert.deepEqual(data.success, false);
       assert.deepEqual(data.error, 'ACCESS DENIED');
+
+      done();
+    });
+  });
+
+
+  /**
+   * Action: "deny"
+   * /**
+   * user policy.
+   *
+   {
+    "group": "user",
+    "permissions": [{
+      "resource": "users",
+      "methods": [
+        "POST",
+        "GET",
+        "PUT"
+      ]
+    }],
+    "action": "deny"
+   }
+   */
+
+
+  describe('Policy testing based on action: "deny', function() {
+    beforeEach(function(done) {
+      acl.config({
+        path: './tests/deny-user-config.json'
+      });
+      done();
+    });
+
+    it('should allow POST operation on /api/user/42', function(done) {
+      req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/api/users/42',
+        params: {
+          id: 42
+        }
+      });
+
+      res = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+
+      req.decoded = {};
+      req.session = {};
+      res.allowed = false;
+
+      req.decoded.role = 'user';
+
+      acl.authorize(req, res, next);
+
+      /**
+       * Traffic should be denied
+       * methods: ["POST","GET","PUT"]
+       * action: "allow"
+       */
+
+      var data = res._getData();
+
+      assert(data, true);
+      assert(typeof data, 'object');
+      assert.deepEqual(data.status, 403);
+      assert.deepEqual(data.success, false);
+      assert.deepEqual(data.error, 'ACCESS DENIED');
+
+      done();
+    });
+
+
+    it('should allow PUT operation on /api/user/42', function(done) {
+      req = httpMocks.createRequest({
+        method: 'PUT',
+        url: '/api/users/42',
+        params: {
+          id: 42
+        }
+      });
+
+      res = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+
+      req.decoded = {};
+      req.session = {};
+      res.allowed = false;
+
+      req.decoded.role = 'user';
+
+      acl.authorize(req, res, next);
+
+      /**
+       * Traffic should be denied
+       * methods: ["POST","GET","PUT"]
+       * action: "allow"
+       */
+
+      var data = res._getData();
+
+      assert(data, true);
+      assert(typeof data, 'object');
+      assert.deepEqual(data.status, 403);
+      assert.deepEqual(data.success, false);
+      assert.deepEqual(data.error, 'ACCESS DENIED');
+
+      done();
+    });
+
+    it('should denie DElETE operation on /api/user/42', function(done) {
+      req = httpMocks.createRequest({
+        method: 'DElETE',
+        url: '/api/users/42',
+        params: {
+          id: 42
+        }
+      });
+
+      res = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+
+      req.decoded = {};
+      req.session = {};
+      res.allowed = false;
+
+      req.decoded.role = 'user';
+
+      acl.authorize(req, res, next);
+
+      /**
+       * Traffic should be allowed
+       * {The policy denies traffic on the below methods, but allow traffic to other methods not specified}
+       * methods: ["POST","GET","PUT"]
+       * action: "allow"
+       */
+
+      var data = res._getData();
+
+      assert(data, true);
+      assert.deepEqual(data.status, 200);
+      assert.deepEqual(data.success, true);
+      assert.deepEqual(data.message, 'ACCESS GRANTED');
 
       done();
     });
