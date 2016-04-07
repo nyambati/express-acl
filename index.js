f9fc43e2eb62925bd70a1971ccd735ffaa08516c
andela-thomas <thomas.nyambati@andela.com>
2016-04-07 10:08:45 +0300

    Adds npm ignore

--- index.js
+++ index.js
@@ -5,27 +5,5 @@
  */
 
 (function() {
-  'use strict';
-  var helper = require('./lib');
-  var rules;
-  var policy;
-
-  module.exports = {
-    config: function(options) {
-      var path, encoding;
-
-      if (options) {
-        path = options.path;
-        encoding = options.encoding;
-      }
-
-      rules = helper.readJson();
-    },
-
-    proctect: function() {
-      var role = req.session.role || req.decoded.role || 'guest';
-
-    }
-
-  };
+  module.exports = require('./lib/nacl');
 })();
