2add24f5723e4e4930f337ac15714eef2ceec759
Nyambati Thomas <thomas.nyambati@andela.com>
2016-04-22 22:30:27 +0300

    Merge Hot fix/unprotected path to develop (#20)
    * adds use strict
    
    * Adds unless for unprotected routes
    
    * Adds tests for the unprotected paths added
    
    * Adds tests for the unprotected paths added

---
+++ tests/nacl.unprotected.path.spec.js
@@ -0,0 +1,60 @@
+var assert = require('assert');
+var acl = require('../');
+var httpMocks = require('node-mocks-http');
+
+describe('Acl middleware for express', function() {
+  var req, res, next;
+
+  describe('Policy based on action: "deny and methods glob "*"', function() {
+    beforeEach(function(done) {
+      acl.config({
+        baseUrl: 'api'
+      });
+      done();
+    });
+
+    it('should deny POST operation on /api/mangoes/42', function(done) {
+      req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/api/oranges'
+      });
+
+      res = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+
+      req.decoded = {};
+      req.session = {};
+
+      req.decoded.role = 'user';
+
+      next = function() {
+        res.send({
+          status: 200,
+          success: true,
+          message: 'ACCESS GRANTED'
+        });
+      };
+
+      acl
+        .authorize
+        .unless({
+          path: [
+            '/api/oranges'
+          ]
+        })(req, res, next);
+
+      var data = res._getData();
+
+      assert(data, true);
+      assert(typeof data, 'object');
+      assert.deepEqual(data.status, 200);
+      assert.deepEqual(data.success, true);
+      assert.deepEqual(data.message, 'ACCESS GRANTED');
+
+      done();
+    });
+
+  });
+
+});
