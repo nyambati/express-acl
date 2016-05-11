6567b326898b4d0b118601afde70a4508f05bc25
Nyambati Thomas <thomas.nyambati@andela.com>
2016-05-11 17:51:39 +0300

    Merge test/unit tests to develop (#42)
    * Adds coday code coverage
    
    *  Refators yml to resolve first before filename
    
    * Exports and changes getRules to allRules and its exported as utils
    
    *  removes test for getRules
    
    *  Includes Lodash for array and string manipulation
    
    * Moves glob and action functions to helpers.js
    
    * Replace ifs with case
    
    * Uses _.isString instead of typeof
    
    * Adds defaults in the case structure
    
    * update default config file
    
    * Update the json changes
    
    * Update README.md
    
    * Added MIT licence
    
    * Moves all behavior testing to behavior folder
    
    *  Adds utils unit tests to test individual method functionlaity
    
    * Cleans test and avoids multple assertion when using deepEqual
    
    * Adds unit test for export to ensure all the core methods are exported

--- tests/behavior/nacl.config.yml.spec.js
+++ tests/behavior/nacl.config.yml.spec.js
@@ -16,25 +16,18 @@
     });
 
     it('should read the yaml file and convert to json', function() {
-      var group = rules[0];
-
+      var expectedRule = [{
+        group: 'user',
+        permissions: [{
+          resource: 'users',
+          methods: ['GET', 'POST', 'DELETE'],
+          action: 'allow'
+        }]
+      }];
       assert(rules, true);
       assert(typeof rules, 'object');
       assert(rules.length, 1);
-      assert.deepEqual(group.group, 'user');
-      assert(Array.isArray(group.permissions), true);
-
-      var policy = group.permissions[0];
-
-      assert(policy.resource, true);
-      assert.deepEqual(policy.resource, 'users');
-      assert(Array.isArray(policy.methods), true);
-      assert(policy.methods, true);
-      assert.deepEqual(policy.methods.indexOf('GET'), 0);
-      assert.deepEqual(policy.methods.indexOf('POST'), 1);
-      assert.deepEqual(policy.methods.indexOf('DELETE'), 2);
-      assert(policy.action, true);
-      assert.deepEqual(policy.action, 'allow');
+      assert.deepEqual(rules, expectedRule);
 
     });
   });
