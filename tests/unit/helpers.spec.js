9730fe69ecbd01d498617d22d8bd50b9480a3c4d
andela-oofoamalu <jessicaprincess61@gmail.com>
2016-05-13 11:04:35 +0100

    Write test for getRole

--- tests/unit/helpers.spec.js
+++ tests/unit/helpers.spec.js
@@ -34,10 +34,7 @@
 
     context('getGroup', function() {
       var res;
-      var mockRole;
       var mockRules;
-      var group;
-      var error;
 
       beforeEach(function(done) {
         mockRules = [{
@@ -49,10 +46,6 @@
           }]
         }];
 
-        error = {
-          message: 'REQUIRED: Group not found'
-        };
-
         res = httpMocks.createResponse(httpMocks.createResponse({
           eventEmitter: require('events').EventEmitter
         }));
@@ -61,16 +54,19 @@
       });
 
       it('Should return a group for a specific rule', function() {
-        mockRole = 'admin';
-        group = helper.getGroup(res, mockRules, mockRole);
+        var mockRole = 'admin';
+        var group = helper.getGroup(res, mockRules, mockRole);
 
         expect(group).to.not.be.empty;
         expect(group.group).to.equal(mockRole);
       });
 
       it('Should return a 404 status', function(done) {
-        mockRole = 'user';
-        group = helper.getGroup(res, mockRules, mockRole);
+        var error = {
+          message: 'REQUIRED: Group not found'
+        };
+        var mockRole = 'user';
+        var group = helper.getGroup(res, mockRules, mockRole);
         var data = res._getData();
 
         expect(res.statusCode).to.equal(404);
@@ -80,12 +76,9 @@
     });
 
     context('getPolicy', function() {
-      var mockResource;
-      var mockGroup;
-
-      beforeEach(function() {
-        mockResource = 'users';
-        mockGroup = {
+      it('Should return the permissions specified', function() {
+        var mockResource = 'users';
+        var mockGroup = {
           group: 'admin',
           permissions: [{
             resource: 'users',
@@ -93,14 +86,57 @@
             action: 'allow'
           }]
         };
-      });
-
-      it('Should return the permissions specified', function() {
         var policy = helper.getPolicy(mockGroup, mockResource);
 
         expect(policy).to.not.be.empty;
         expect(policy).to.have.property('methods');
         expect(policy).to.have.property('action');
+      });
+    });
+
+    context('getRole', function() {
+      var res;
+
+      beforeEach(function(done) {
+        res = httpMocks.createResponse(httpMocks.createResponse({
+          eventEmitter: require('events').EventEmitter
+        }));
+        done();
+      });
+
+      it('Should return the role', function() {
+        var session = {};
+        var decoded = {
+          role: 'user'
+        };
+        var role = helper.getRole(res, decoded, session);
+
+        expect(role).to.not.be.empty;
+        expect(role).to.equal(decoded.role);
+      });
+
+      it('Should return the role when session exits and decoded is empty', function() {
+        var session = {
+          role: 'admin'
+        };
+        var decoded = {};
+        var role = helper.getRole(res, decoded, session);
+
+        expect(role).to.not.be.empty;
+        expect(role).to.equal(session.role);
+      });
+
+      it('Should respond with 404', function() {
+        var error = {
+          message: 'REQUIRED: Role not found'
+        };
+        var session = {};
+        var decoded = {};
+        var role = helper.getRole(res, decoded, session);
+        var data = res._getData();
+
+        expect(res.statusCode).to.equal(404);
+        expect(data.message).to.equal(error.message);
       });
     });
   });
