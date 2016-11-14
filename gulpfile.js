42d037de6684f29d07c58833819a8d516ce93bc1
Thomas Nyambati <thomas.nyambati@andela.com>
2016-11-14 21:02:53 +0300

    chore(make): add make file to the project - add make file to abstract development commands - removes unnecesary gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@
   var gulp = require('gulp');
   var plugins = require('gulp-load-plugins')();
   var paths = {
-      tests: ['tests/**/**.js'],
-      scripts: ['lib/**', 'tests/**','!tests/config/**.yml']
+    tests: ['tests/**/**.js'],
+    scripts: ['lib/**', 'tests/**', '!tests/config/**.yml']
   };
 
   /**
@@ -13,49 +13,43 @@
    */
 
   gulp.task('lint', function() {
-      return gulp.src(paths.scripts)
-          .pipe(plugins.jshint())
-          .pipe(plugins.jshint.reporter('jshint-stylish', { verbose: true }));
+    return gulp.src(paths.scripts)
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('jshint-stylish', { verbose: true }));
   });
-
 
   /**
    * Gulp task to run tests
    *
    */
 
-  gulp.task('test',['pre-test'], function() {
-      return gulp.src(paths.tests, { read: false })
-          // The reporter can be changed to preferable one
-          .pipe(plugins.mocha())
-          // Creating the reports after tests ran
-          .pipe(plugins.istanbul.writeReports())
-          // Enforce a coverage of at least 90%
-          .pipe(plugins.istanbul.enforceThresholds({
-             thresholds: {global: 90}
-           }));
+  gulp.task('test', ['pre-test'], function() {
+    return gulp.src(paths.tests, { read: false })
+      // The reporter can be changed to preferable one
+      .pipe(plugins.mocha())
+      // Creating the reports after tests ran
+      .pipe(plugins.istanbul.writeReports())
+      // Enforce a coverage of at least 90%
+      .pipe(plugins.istanbul.enforceThresholds({
+        thresholds: { global: 90 }
+      }));
   });
-
 
   /**
    * Watch for changes and lint in all ours scripts and lints
    */
   gulp.task('watch', function() {
-      gulp.watch(paths.scripts, ['lint']);
+    gulp.watch(paths.scripts, ['lint']);
   });
 
-
-  gulp.task('pre-test', function () {
-  return gulp.src(['lib/**/*.js','index.js'])
-    // Covering files
-    .pipe(plugins.istanbul())
-    // Force `require` to return covered files
-    .pipe(plugins.istanbul.hookRequire());
+  gulp.task('pre-test', function() {
+    return gulp.src(['lib/**/*.js', 'index.js'])
+      // Covering files
+      .pipe(plugins.istanbul())
+      // Force `require` to return covered files
+      .pipe(plugins.istanbul.hookRequire());
   });
 
-  gulp.task('build-image', plugins.shell.task([
-    'docker build -t thomasnyambati/constable .'
-  ]));
   /**
    * Start gulp
    */
